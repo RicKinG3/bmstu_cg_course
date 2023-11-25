8958be9bd789ba35b2474da0ced488ae4dc2d538
Max <mvolnyaga@gmail.com>
2023-11-25 19:35:04 +0300

    del prog

--- course/prog/CourseWork/loader/camera/camera_loader.h
+++
@@ -1,8 +0,0 @@
-#ifndef LAB_03_CAMERA_LOADER_H
-#define LAB_03_CAMERA_LOADER_H
-
-#include "../base_loader.h"
-
-class CameraLoader : public BaseLoader {};
-
-#endif //LAB_03_CAMERA_LOADER_H
--- course/prog/CourseWork/loader/model/model_loader.h
+++
@@ -1,9 +0,0 @@
-#ifndef LAB_03_MODEL_LOADER_H
-#define LAB_03_MODEL_LOADER_H
-
-#include "../base_loader.h"
-
-
-class ModelLoader : public BaseLoader {};
-
-#endif //LAB_03_MODEL_LOADER_H
